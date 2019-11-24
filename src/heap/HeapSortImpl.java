292b5987c21765ea4dcef864ae7d75ef938c2aad
mukitul <mukitulislamratul@gmail.com>
2019-11-25 01:33:34 +0600

    heap-sort impl not working!

--- src/heap/HeapSortImpl.java
+++ src/heap/HeapSortImpl.java
@@ -7,7 +7,7 @@ public class HeapSortImpl {
 		int ar[];
 
 		HeapSort(int size, int[] a){
-			n=size;
+			n=size-1;
 			ar = a;
 		}
 
@@ -25,14 +25,12 @@ public class HeapSortImpl {
 			ar[b] = tmp;
 		}
 
-		void Max_Heapify(int i){
+		void Max_Heapify(int i, int n){
 			int l = leftChild(i);
 			int r = rightChild(i);
 			int largest=i;
 			if(l<=n && ar[l]>ar[i]) {
 				largest = l;
-			}else {
-				largest = i;
 			}
 
 			if(r<=n && ar[r]>ar[largest]) {
@@ -41,24 +39,31 @@ public class HeapSortImpl {
 
 			if(largest != i) {
 				swap(i,largest);
-				Max_Heapify(largest);
+				Max_Heapify(largest,n);
 			}
 		}
 
 		void buildMaxHeap() {
 
-			for(int i=Math.floorDiv(n, 2)-1; i>=0; i--) {
-				Max_Heapify(i);
+			for(int i=Math.floorDiv(n, 2); i>=0; i--) {
+				Max_Heapify(i,n);
 			}
 		}
 
 		void heapSort() {
 			buildMaxHeap();
-			//n=n-1;
-			for(int i=ar.length-1;i>0;i--) {
+			int size = ar.length;
+			for(int i=ar.length-1;i>=1;i--) {
+				
 				swap(i,0);
-				n = n-2;
-				Max_Heapify(i);
+				System.out.println("i  "+i);
+				size = size - 1;
+				Max_Heapify(1,size);
+				System.out.println("size  "+size);
+			}
+			
+			for(int i=0;i<ar.length;i++) {
+				System.out.print(ar[i]+" ");
 			}
 		}
 
@@ -69,13 +74,13 @@ public class HeapSortImpl {
 		}
 	}
 	public static void main(String[] args) {
-		int ar[] = {4,10,3,5,1};
-		//int ar[] = {12, 11, 13, 5, 6, 7}; 
+		//int ar[] = {4,10,3,5,1};
+		int ar[] = {12, 11, 13, 5, 6, 7}; 
 		int a=ar.length;
 		HeapSort hs = new HeapSort(a,ar);
 
 		hs.heapSort();
-		hs.printRes();
+		//hs.printRes();
 	}
 
 }
