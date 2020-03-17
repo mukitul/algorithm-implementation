b96f8ebf80647b875ea54ad046079435df4e9f0c
mukitul <mukitulislamratul@gmail.com>
2020-03-18 00:42:10 +0600

    getting error

--- src/kadanes_algorithm/KadanesImpl.go
+++ src/kadanes_algorithm/KadanesImpl.go
@@ -1,17 +1,20 @@
 package main
 
 import "fmt"
+
 func max(int a, int b) int{
 	if a>b {
 		return a;
 	}
 	return b;
 }
+
 func kadane(ar []int, int size) int {
 	var i int;
 	var max_curr int;
 	var max_glo int;
-	max_curr = max_glo = ar[0];
+	max_curr = ar[0];
+	max_glo = ar[0];
 	for i := 0; i < size; i++ {
 		max_curr = max(ar[i], ar[i]+max_curr);
 
@@ -27,5 +30,5 @@ func main() {
 	var  ar = []int {-2,3,2,-1};
 	var res int;
 	res = kadane(ar,4);
-	fmt.Println(res);
+	fmt.Println("%d ",res);
 }
