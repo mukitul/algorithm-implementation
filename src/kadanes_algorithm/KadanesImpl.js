c80597d2536929578e634065683d5985014534c3
mukitul <mukitulislamratul@gmail.com>
2020-03-17 22:25:17 +0600

    kadane in js implemnted

--- src/kadanes_algorithm/KadanesImpl.js
+++ src/kadanes_algorithm/KadanesImpl.js
@@ -1,1 +1,20 @@
-console.log("hello");
+
+let max = (a,b) => {
+    if(a>b) return a;return b;
+};
+
+function kadane(ar) {
+    let max_curr=ar[0];
+    let max_glo=ar[0];
+    for (let index = 0; index < ar.length; index++) {
+        max_curr = max(ar[index],ar[index]+max_curr);
+        
+        if(max_curr>max_glo) max_glo=max_curr;
+    }
+
+    return max_glo;
+}
+
+let ar=[-2,3,2,-1];
+let res = kadane(ar);
+console.log(res);
