90ab4d765452da68b6b5bbf38ad4c8ffe6e22dc0
mukitul <mukitulislamratul@gmail.com>
2020-03-17 21:19:21 +0600

    kadanes' algo

--- src/kadanes_algorithm/KadanesImpl.java
+++ src/kadanes_algorithm/KadanesImpl.java
@@ -2,37 +2,40 @@ package kadanes_algorithm;
 
 public class KadanesImpl {
 
-	static class Solution{
-		Solution(){
-			
-		}
-		
+	static class Solution {
+
 		int kadane(int[] ar) {
-			if(ar.length==0) return -1;
+			if (ar.length == 0)
+				return -1;
+
 			int max_curr;
 			int max_global;
 			max_global = max_curr = ar[0];
-			for(int i=1;i<ar.length;i++) {
-				max_curr = Math.max(max_curr, max_curr+ar[i]);
-				
-				if(max_curr>max_global) {
+
+			for (int i = 1; i < ar.length; i++) {
+				max_curr = Math.max(max_curr, max_curr + ar[i]);
+
+				if (max_curr > max_global) {
 					max_global = max_curr;
 				}
 			}
-			
+
 			return max_global;
 		}
 	}
+
 	public static void main(String[] args) {
-		//int[] ar = {0,1,2,3,4,5};
-		//int[] ar = {0};
-		//int[] ar = {0,-1};
-		//int[] ar = {};
-		//int[] ar = {-1,-5,0,0,-1};
-		int[] ar = {-1,-5,25,0,-1};
+		// int[] ar = {0,1,2,3,4,5}; //case-1
+		// int[] ar = {0}; //case-2
+		// int[] ar = {0,-1}; //case-3
+		// int[] ar = {}; //case-4
+		// int[] ar = {-1,-5,0,0,-1}; //case-5
+		int[] ar = { -1, -5, 25, 0, -1 }; // case-6
+
 		Solution sln = new Solution();
+
 		int res = sln.kadane(ar);
-		System.out.println("max sub-array sum:"+res);
+		System.out.println("max sub-array sum:" + res);
 	}
 
 }
