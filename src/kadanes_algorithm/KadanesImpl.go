959e3bfca9bb9bc29c7257658c05c82b553c4ecb
mukitul <mukitulislamratul@gmail.com>
2020-03-18 00:35:13 +0600

    implting in go

---
+++ src/kadanes_algorithm/KadanesImpl.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+func max(int a, int b) int{
+	if a>b {
+		return a;
+	}
+	return b;
+}
+func kadane(ar []int, int size) int {
+	var i int;
+	var max_curr int;
+	var max_glo int;
+	max_curr = max_glo = ar[0];
+	for i := 0; i < size; i++ {
+		max_curr = max(ar[i], ar[i]+max_curr);
+
+		if max_curr>max_glo {
+			max_glo=max_curr;
+		}
+	}
+
+	return max_glo;
+}
+
+func main() {
+	var  ar = []int {-2,3,2,-1};
+	var res int;
+	res = kadane(ar,4);
+	fmt.Println(res);
+}
