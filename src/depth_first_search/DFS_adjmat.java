439c2d05c9be3ed60ab988c236451d05cef96ac0
mukitul <mukitulislamratul@gmail.com>
2020-03-19 00:26:59 +0600

    dfs adj-mat java implemented

---
+++ src/depth_first_search/DFS_adjmat.java
@@ -0,0 +1,45 @@
+package depth_first_search;
+
+public class DFS_adjmat {
+	
+	static class DFS {
+		boolean visited[];
+		int vertex;
+		int mat[][];
+
+		DFS(int v) {
+			vertex = v;
+			mat = new int[vertex][vertex];
+			visited = new boolean[vertex];
+		}
+		
+		void addEdge(int u, int v){
+			mat[u][v]=1;
+		}
+		
+		void dfs(int v){
+			System.out.println(v);
+			visited[v]=true;
+			for(int i=0;i<vertex;i++){
+				if(mat[v][i]==1 && !visited[i]){
+					dfs(i);
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int vertex = 4;
+		DFS d = new DFS(vertex);
+		d.addEdge(0, 1);
+		d.addEdge(0, 2);
+		d.addEdge(2, 0);
+		d.addEdge(1, 2);
+		d.addEdge(2, 3);
+		d.addEdge(3, 3);
+		d.dfs(2);
+		
+	}
+
+}
