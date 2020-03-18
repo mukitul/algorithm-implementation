ad9b3e0b34a7a4646657a113cbff213462717e8c
mukitul <mukitulislamratul@gmail.com>
2020-03-18 20:39:48 +0600

    implemented in go

--- src/kadanes_algorithm/KadanesImpl.go
+++ src/kadanes_algorithm/KadanesImpl.go
@@ -2,33 +2,33 @@ package main
 
 import "fmt"
 
-func max(int a, int b) int{
-	if a>b {
-		return a;
+func max(a int, b int) int {
+	if a > b {
+		return a
 	}
-	return b;
+	return b
 }
 
-func kadane(ar []int, int size) int {
-	var i int;
+func kadane(ar []int, size int) int {
+	//var i int;
 	var max_curr int;
 	var max_glo int;
-	max_curr = ar[0];
-	max_glo = ar[0];
+	max_curr = ar[0]
+	max_glo = ar[0]
 	for i := 0; i < size; i++ {
-		max_curr = max(ar[i], ar[i]+max_curr);
+		max_curr = max(ar[i], ar[i]+max_curr)
 
-		if max_curr>max_glo {
-			max_glo=max_curr;
+		if max_curr > max_glo {
+			max_glo = max_curr
 		}
 	}
 
-	return max_glo;
+	return max_glo
 }
 
 func main() {
-	var  ar = []int {-2,3,2,-1};
-	var res int;
-	res = kadane(ar,4);
-	fmt.Println("%d ",res);
+	var ar = []int{-2, 3, 2, -1}
+	var res int
+	res = kadane(ar, 4)
+	fmt.Println(res)
 }
