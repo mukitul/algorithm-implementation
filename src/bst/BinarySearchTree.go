79a34ef21bca4fe9faade54b05c7324eb1929dbf
mukitul <mukitulislamratul@gmail.com>
2020-04-04 21:05:47 +0600

    bst in go ---- not implemented

---
+++ src/bst/BinarySearchTree.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	left  *Node
+	right *Node
+	val   int
+}
+
+type BinarySearchTree struct {
+	root *Node
+}
+
+func main() {
+	fmt.Print("not implemented...!")
+}
