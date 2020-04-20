0ecbd9f255add347afa83790dd477f18deb64bd3
mukitul <mukitulislamratul@gmail.com>
2020-04-20 10:36:28 +0600

    binary serch reccur

--- src/binary_search/BinarySearchImpl.c
+++ src/binary_search/BinarySearchImpl.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+// reccur
+int binarySearchReccur(int ar[], int left, int right, int x)
+{
+    if (right >= left)
+    {
+        int mid = left + (right - left) / 2;
+        if (ar[mid] == x)
+        {
+            return mid;
+        }
+        else if (ar[mid] > x)
+        {
+            return binarySearchReccur(ar, left, mid - 1, x);
+        }
+        else
+        {
+            return binarySearchReccur(ar, mid + 1, right, x);
+        }
+    }
+
+    return -1;
+}
+
 // iterative
 int binarySearch(int ar[], int len, int x)
 {
@@ -28,7 +51,10 @@ int binarySearch(int ar[], int len, int x)
 int main()
 {
     int ar[5] = {1, 2, 3, 4, 5};
-    int res = binarySearch(ar, 5, 5);
-    printf("%d\n", res);
+    int res1 = binarySearch(ar, 5, 6);
+    printf("%d\n", res1);
+
+    int res2 = binarySearchReccur(ar, 0, 4, 0);
+    printf("%d\n", res2);
     return 0;
 }
