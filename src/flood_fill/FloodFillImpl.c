564034a8b47025438cf86f2d7f7719c49a34bab5
mukitul <mukitulislamratul@gmail.com>
2020-04-21 16:51:51 +0600

    tail recursion--floodfill in c

--- src/flood_fill/FloodFillImpl.c
+++ src/flood_fill/FloodFillImpl.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-int row, col;
-
-int floodFill(char graph[][col])
-{
-    int count = 0;
-    int i, j;
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < col; i++)
-        {
-            if (graph[i][j] == 'l')
-            {
-                count += dfs(graph, i, j);
-            }
-        }
-    }
-}
+int row = 5;
+int col = 4;
 
 int dfs(char graph[][col], int i, int j)
 {
-    if (i < 0 || j < 0 || i > row || j > col)
+    if (i < 0 || j < 0 || i > row || j > col || graph[i][j] == 'w')
         return 0;
 
     graph[i][j] = 'w';
@@ -34,8 +19,36 @@ int dfs(char graph[][col], int i, int j)
 
     return 1;
 }
+
+int floodFill(char graph[][col])
+{
+    int count = 0;
+    int i, j;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (graph[i][j] == 'l')
+            {
+                count += dfs(graph, i, j);
+            }
+        }
+    }
+    return count;
+}
+
 int main()
 {
-
+    /* 
+    'w', 'w', 'w', 'l'
+    'l', 'l', 'w', 'w'
+    'w', 'l', 'l', 'w'
+    'w', 'w', 'w', 'w'
+    'w', 'w', 'l', 'w'
+     */
+    char graph[5][4] = {{'w', 'w', 'w', 'l'}, {'l', 'l', 'w', 'w'}, {'w', 'l', 'l', 'w'}, {'w', 'w', 'w', 'w'}, {'w', 'w', 'l', 'w'}};
+    int res = floodFill(graph);
+    printf("%d\n", res);
     return 0;
 }
